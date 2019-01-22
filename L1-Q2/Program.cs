527462f884f216b340673aa2a62f4b91a429cb42
sagar26197@gmail.com <sagar26197@gmail.com>
2019-01-22 18:24:42 -0500

    V2

--- L1-Q2/Program.cs
+++ L1-Q2/Program.cs
@@ -6,7 +6,19 @@ namespace L1_Q2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter any Number");
+            int i;
+            i = int.Parse(Console.ReadLine());
+            if (i % 2 == 0) 
+            {
+                Console.WriteLine("Entered number is an even number");
+                Console.ReadKey(true);
+            }
+            else
+            {
+                Console.WriteLine("Entered number is an odd number");
+                Console.ReadKey(true);
+            }
         }
     }
 }
